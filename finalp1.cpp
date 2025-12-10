3b9f336efe9085da259b6e7b4e11abf282556a87
arayapudi626 <rayapudianisha@gmail.com>
2025-12-09 19:59:00 -0800

    Milestone 4

--- finalp1.cpp
+++ finalp1.cpp
@@ -1,6 +1,5 @@
 //COMSC 210 | Final Part 1 | Anisha Rayapudi
 //IDE used: VS 2022
-
 #include <iostream>
 #include <string>
 #include <map>
@@ -8,20 +7,20 @@
 
 using namespace std;
 
+//Milestone 3:
 void portsInrange(const map<string, int>& airport, int low, int high){
     cout << "Airports with traffic in range [" << low << ", " << high << "]: " << endl;
     for (const auto& val : airport){
-    if (val.second >= low && val.second <=high){
+        if (val.second >= low && val.second <=high){
         cout << val.first << " " << val.second << endl;
+        }
     }
-}
-
-
+    cout << endl;
 }
 
 
 int main(){
-//Milestone 1:
+    //Milestone 1:
     map<string, int> airport;
     ifstream fin("210-final-1-FA25.txt");
     if (!fin) {
@@ -31,35 +30,40 @@ int main(){
 
     string origin;
     string dest;
+
     while (fin >> origin >> dest){
         airport[origin]++;
         airport[dest]++;
     }
     fin.close();
 
-cout << "All airports traffic counts: " << endl;
-for (const auto& val : airport){
-    cout << val.first << " " << val.second << endl;
-
-}
-
-
-//Milestone 2:
-int max = 0;
-for (const auto&val : airport){
-    if (val.second > max){
-        max = val.second;
-    }
-}
-cout << "Busiest airport(s) with count " << max << ": " << endl;
-for (const auto&val : airport){
-    if (val.second == max){
+    cout << "All airports traffic counts: " << endl;
+    for (const auto& val : airport){
         cout << val.first << " " << val.second << endl;
-    }
-}
 
-portsInrange(airport, 5, 8);
-portsInrange(airport, 9, 12);
+    }
+    cout << endl;
+
+
+    //Milestone 2:
+    int max = 0;
+    for (const auto&val : airport){
+        if (val.second > max){
+            max = val.second;
+        }
+    }
+
+    cout << "Busiest airport(s) with count " << max << ": " << endl;
+    for (const auto&val : airport){
+        if (val.second == max){
+            cout << val.first << " " << val.second << endl;
+        }
+    }
+    cout << endl;
+
+    //Milestone 3:
+    portsInrange(airport, 5, 8);
+    portsInrange(airport, 9, 12);
 
 
     return 0;
